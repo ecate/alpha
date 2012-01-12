c77bc60032bdb5e7caadc24172fb270c62d6df00
Marine Serré <marine.serre@gmail.com>
2012-01-12 14:45:10 +0100

    compatibility 1.8

--- app/mailers/demande_spa_mailer.rb
+++ app/mailers/demande_spa_mailer.rb
@@ -1,5 +1,5 @@
 class DemandeSpaMailer < ActionMailer::Base
-  default from: "marine.serre@gmail.com"
+  default :from => "marine.serre@gmail.com"
 
   #pour une mission données, on envoie à tous les personnels un compte-rendu
   def envoi_demande_spa(mi)
